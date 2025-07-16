d682a7f4338d856ca6850b4db175691b28087ffb
Carla Daniela Nascimento <carla.danielaju@gmail.com>
2025-07-16 14:04:51 -0300

    Desafio de POO concluído!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Testando iPhone:");
+Iphone iphone = new Iphone("12345", "iPhone 14", "111222333", 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
+
+Console.WriteLine("\nTestando Nokia:");
+Nokia nokia = new Nokia("67890", "Nokia 3310", "444555666", 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Snake");
